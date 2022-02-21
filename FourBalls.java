dc980ed8a95a63769ecb66757da0b25437616831
Hemalatha900 <57185451+Hemalatha900@users.noreply.github.com>
2022-02-20 19:22:47 -0800

    Update FourBalls.java
    changed the program

--- FourBalls.java
+++ FourBalls.java
@@ -1,34 +1,57 @@
 import processing.core.PApplet;
-public class TryProcessing extends PApplet {
-  public static final int H=640;
-  public static final int W=480;
-  public static final int D=20;
-  int x=0;
-  int dspeed=0;
-  public static void main(String args[]){
-    PApplet.main("TryProcessing",args);
-  }
-  @Override
-  public void settings(){
-    super.settings();
-    size(H,W);
-  }
-  @Override
-  public void draw(){
-    //paintWhite();
-    drawCircle();
-  }
-  public void paintWhite(){
-    background(255);
-  }
-  public void drawCircle(){
-        int Height=30;
-        for(int i=0;i<4;i++){
-             Height+=100;
-             ellipse(x,Height,D,D);
-             x++;
-        }
 
-  }
-  
+public class FourBalls extends PApplet {
+    public int height=680;
+    public int width=680;
+    public static int x_axis=0;
+    public Balls ball_one=new Balls(x_axis,100,1,this);
+    public Balls ball_two=new Balls(x_axis,200,3,this);
+    public Balls ball_three=new Balls(x_axis,300,5,this);
+    public Balls ball_four=new Balls(x_axis,400,10,this);
+    public static void main(String args[]){
+        PApplet.main("FourBalls",args);
+    }
+    @Override
+    public void settings(){
+        super.settings();
+        size(height,width);
+
+    }
+    @Override
+    public void setup(){
+    }
+    @Override
+    public void draw(){
+
+       ball_one.DrawBall();
+       ball_two.DrawBall();
+       ball_three.DrawBall();
+       ball_four.DrawBall();
+    }
+
+}
+
+class Balls {
+
+    public  int x_axis;
+    public  int y_axis;
+    public  int speed;
+    public  int diameter=20;
+    public PApplet Papplet ;
+    public Balls(int x_axis, int y_axis, int speed, PApplet Papplet){
+        this.x_axis=x_axis;
+        this.y_axis=y_axis;
+        this.speed=speed;
+        this.Papplet=Papplet;
+    }
+    public void DrawBall(){
+        Papplet.ellipse(x_axis,y_axis,diameter,diameter);
+        IncreaseSpeedofBall();
+
+    }
+
+    private void IncreaseSpeedofBall() {
+        x_axis += speed;
+
+    }
 }
